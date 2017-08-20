d167344c2f7d4a93f3845efd365cd9035ac5ede2
Jason Graziano <mrehawk82@gmail.com>
2017-08-19 21:55:22 -0400

    loadUserData function updated, and userData object added.  Also, there is a new function called "initialize()" for checking new user sessions

--- kids-game/js/main.js
+++ kids-game/js/main.js
@@ -2,70 +2,91 @@
 // http://go.microsoft.com/fwlink/?LinkId=232509
 
 (function () {
-	"use strict";
+    "use strict";
 
-	var app = WinJS.Application;
-	var activation = Windows.ApplicationModel.Activation;
-	var isFirstActivation = true;
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    var isFirstActivation = true;
 
-	var ViewManagement = Windows.UI.ViewManagement;
-	var ApplicationViewWindowingMode = ViewManagement.ApplicationViewWindowingMode;
-	var ApplicationView = ViewManagement.ApplicationView;
+    var ViewManagement = Windows.UI.ViewManagement;
+    var ApplicationViewWindowingMode = ViewManagement.ApplicationViewWindowingMode;
+    var ApplicationView = ViewManagement.ApplicationView;
 
-	app.onactivated = function (args) {
-		if (args.detail.kind === activation.ActivationKind.voiceCommand) {
-			// TODO: Handle relevant ActivationKinds. For example, if your app can be started by voice commands,
-			// this is a good place to decide whether to populate an input field or choose a different initial view.
-		}
-		else if (args.detail.kind === activation.ActivationKind.launch) {
-			// A Launch activation happens when the user launches your app via the tile
-			// or invokes a toast notification by clicking or tapping on the body.
-			if (args.detail.arguments) {
-				// TODO: If the app supports toasts, use this value from the toast payload to determine where in the app
-				// to take the user in response to them invoking a toast notification.
-			}
-			else if (args.detail.previousExecutionState === activation.ApplicationExecutionState.terminated) {
-				// TODO: This application had been suspended and was then terminated to reclaim memory.
-				// To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
-				// Note: You may want to record the time when the app was last suspended and only restore state if they've returned after a short period.
-			}
-		}
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.voiceCommand) {
+            // TODO: Handle relevant ActivationKinds. For example, if your app can be started by voice commands,
+            // this is a good place to decide whether to populate an input field or choose a different initial view.
+        }
+        else if (args.detail.kind === activation.ActivationKind.launch) {
+            // A Launch activation happens when the user launches your app via the tile
+            // or invokes a toast notification by clicking or tapping on the body.
+            if (args.detail.arguments) {
+                // TODO: If the app supports toasts, use this value from the toast payload to determine where in the app
+                // to take the user in response to them invoking a toast notification.
+            }
+            else if (args.detail.previousExecutionState === activation.ApplicationExecutionState.terminated) {
+                // TODO: This application had been suspended and was then terminated to reclaim memory.
+                // To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+                // Note: You may want to record the time when the app was last suspended and only restore state if they've returned after a short period.
+            }
+        }
 
-		if (!args.detail.prelaunchActivated) {
-			// TODO: If prelaunchActivated were true, it would mean the app was prelaunched in the background as an optimization.
-			// In that case it would be suspended shortly thereafter.
-			// Any long-running operations (like expensive network or disk I/O) or changes to user state which occur at launch
-			// should be done here (to avoid doing them in the prelaunch case).
-			// Alternatively, this work can be done in a resume or visibilitychanged handler.
-		}
+        if (!args.detail.prelaunchActivated) {
+            // TODO: If prelaunchActivated were true, it would mean the app was prelaunched in the background as an optimization.
+            // In that case it would be suspended shortly thereafter.
+            // Any long-running operations (like expensive network or disk I/O) or changes to user state which occur at launch
+            // should be done here (to avoid doing them in the prelaunch case).
+            // Alternatively, this work can be done in a resume or visibilitychanged handler.
+        }
 
-		if (isFirstActivation) {
-			// TODO: The app was activated and had not been running. Do general startup initialization here.
-			document.addEventListener("visibilitychange", onVisibilityChanged);
-			args.setPromise(WinJS.UI.processAll());
-			ApplicationView.preferredLaunchWindowingMode = ApplicationViewWindowingMode.fullScreen;
+        if (isFirstActivation) {
+            // TODO: The app was activated and had not been running. Do general startup initialization here.
+            document.addEventListener("visibilitychange", onVisibilityChanged);
+            args.setPromise(WinJS.UI.processAll());
+            ApplicationView.preferredLaunchWindowingMode = ApplicationViewWindowingMode.fullScreen;
             UI.startup();//starts general program here
-		}
+        }
 
-		isFirstActivation = false;
-	};
+        isFirstActivation = false;
+    };
 
-	function onVisibilityChanged(args) {
-		if (!document.hidden) {
-			// TODO: The app just became visible. This may be a good time to refresh the view.
-		}
-	}
+    function onVisibilityChanged(args) {
+        if (!document.hidden) {
+            // TODO: The app just became visible. This may be a good time to refresh the view.
+        }
+    }
 
-	app.oncheckpoint = function (args) {
-		// TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
-		// You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
-		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
-	};
-	var UI;
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+        // You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+        // If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+    };
+    var UI, userData;
 
-	UI = {
+    userData = {
+        level: 0,
+        candy: 0,
+        color: "",
+        special: "",
+        trophy: ""
+    };
+
+    UI = {
+        initialize: () => {
+            var uData = localStorage.getItem("userData");
+            if (!uData || uData === null) {
+                localStorage.setItem("userData", JSON.stringify(userData));//creating new user's info
+            }
+            /*
+            var xx = localStorage.getItem("userData");
+            if (xx) {
+                var xxx = JSON.parse(xx);
+            }
+            console.log(xxx.candy);
+            */
+        },
 	    createEle: (x) => { return document.createElement(x); },
-	    startup: () => {
+	    startup: () => {// loading initialization data here
 	        var startBTN = UI.createEle("button");
 	        //console.log("startup");
 	        startBTN.innerHTML = "Start";
@@ -73,10 +94,10 @@
 	        startBTN.onclick = UI.beginningGame(startBTN);
 
 	        hub.appendChild(startBTN);
-
+	        UI.initialize();
 	        setTimeout(() => { startBTN.id = "startBTN_full" }, 500);
 	    },
-	    beginningGame: (startBTN) => {//putting away the start button and then removing it, then triggering the loading screen; possibly ansycroniously loading userdata check here?
+	    beginningGame: (startBTN) => {//putting away the start button and then removing it, then triggering the loading screen.
 	        return () => {
 	            //console.log(startBTN);
 	            startBTN.id = "startBTN";
@@ -118,9 +139,14 @@
             }, 500);
 	    },
         loadUserData: (ldScrn, ldGif) => {//here is where the game checks the user data
-
-                console.log("loading user status..");
-                setTimeout(() => { UI.killLoadScreen(ldScrn, ldGif); }, 2000);
+            var uData = localStorage.getItem("userData");
+            
+            if (uData) {
+                var uuu = JSON.parse(uData);
+            }
+            
+            console.log(uuu.level);
+            //setTimeout(() => { UI.killLoadScreen(ldScrn, ldGif); }, 2000);
             
 	    }
 	};
